7ea7115a8edba5c01e7b64defaba4624b8f7a813
iklilboy <iklilmuhammad002@gmail.com>
2023-07-01 19:15:16 +0700

    Lanjutan PBKK

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,12 +15,13 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link href="{{ asset('css/pizza.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -29,6 +30,12 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+                    @can('isResto') 
+<li class="nav-item"><a class="nav-link"  
+href="{{ url('/resto/pizza') }}">Pizza</a></li> 
+<li class="nav-item"><a class="nav-link"  
+href="{{ url('/resto/user') }}">User</a></li> 
+@endcan
                     <ul class="navbar-nav me-auto">
 
                     </ul>
